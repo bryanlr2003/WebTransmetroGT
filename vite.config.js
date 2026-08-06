d714d061a790038c96cf0d6c9ee44ba1b1390b46
bryanlr2003 <blucasr2@miumg.edu.gt>
2026-08-05 23:33:07 -0600

    BSLR: configurar base del proyecto Transmetro GT

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuracion basica de Vite para ejecutar React en desarrollo y produccion.
 export default defineConfig({
   plugins: [react()],
 })
